ac35558cf4206bf4acd30754307234abad18727d
josh <jeycuff@gmail.com>
2019-08-11 21:30:37 +0200

    new

---
+++ calc.js
@@ -0,0 +1,18 @@
+
+const result = 0;
+
+function add(a, b) {
+    return result = a + b;    
+}
+
+function divide(a, b) {
+
+}
+
+function subtract(a, b) {
+
+}
+
+function multiply(a, b) {
+    
+}
