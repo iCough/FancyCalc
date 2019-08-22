c68520c3503dbd3e3ced47cc7ef54b7217cc507e
josh <jeycuff@gmail.com>
2019-08-22 17:38:11 +0200

    added numblock + for-loop over all numbers (incomplete)

--- calc.js
+++ calc.js
@@ -1,16 +1,38 @@
+console.log("Status: js-file is connected!");
 
+const nums = [1,2,3,4,5,6,7,8,9,0];
 
+for (let key of Object.keys(nums)) {
+    console.log(document.querySelectorAll(".button" + nums[key]));
+}
+
+/** 
+ * Eine for-loop machen, in der der gedrückte button mit der 
+ * jeweiligen Zahl abgeglichen wird.
+*/
 const one = document.querySelector(".button1");
-//const one = document.getElementById("one");
+const two = document.querySelector(".button2");
+const three = document.querySelector(".button3");
+const four = document.querySelector(".button4");
+const five = document.querySelector(".button5");
+const six = document.querySelector(".button6");
+const seven = document.querySelector(".button7");
+const eigth = document.querySelector(".button8");
+const nine = document.querySelector(".button9");
+const zero = document.querySelector(".button0");
 
-console.log(one);
 
 /**
  * BUTTON CONTROL
  */
-one.addEventListener("click", function(sth) {
-    
-    console.log("click!");
+one.addEventListener("click", function() {    
+    console.log("1");
+});
+two.addEventListener("click", function() {    
+    console.log("2");
+});
+three.addEventListener("click", function() {    
+    console.log("3");
 });
  
 
