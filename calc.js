1dbb7809948513e7d09ab783374fc4b4e792dd3a
josh <jeycuff@gmail.com>
2019-08-24 13:24:54 +0200

    eventHandler for numPad

--- calc.js
+++ calc.js
@@ -1,10 +1,30 @@
-console.log("Status: js-file is connected!");
 
-const nums = [1,2,3,4,5,6,7,8,9,0];
 
-for (let key of Object.keys(nums)) {
-    console.log(document.querySelectorAll(".button" + nums[key]));
+const numPad = document.getElementById('numpad');
+numPad.addEventListener("click", numPadClickHandler());
+
+
+
+function numPadClickHandler(event) {
+    const clickedButton = event.target;
+    const num = parseInt(clickedButton.textContent);
+    console.log(num);
+    return num;
 }
+
+// function pressedNumber() {
+
+// for (let number of nums) {
+
+//     const pressedNum = document.querySelector(".button" + number.innerHTML).innerHTML;
+
+//     // if pressed button is the same number as the one iterated through
+//     // then print that number
+//     if (pressedNum === number) {
+//         console.log(pressedNum);
+//     }
+// }
+// }
 
 /** 
  * Eine for-loop machen, in der der gedrückte button mit der 
@@ -22,31 +42,9 @@ const nine = document.querySelector(".button9");
 const zero = document.querySelector(".button0");
 
 
-/**
- * BUTTON CONTROL
- */
-one.addEventListener("click", function() {    
-    console.log("1");
-});
-two.addEventListener("click", function() {    
-    console.log("2");
-});
-three.addEventListener("click", function() {    
-    console.log("3");
-});
- 
-
-
-/**
- * FUNCTIONS
- */
-
-const result = 0;
-
-var x = 
 
 function add(a, b) {
-    return result = a + b;    
+    return result = a + b;
 }
 
 function divide(a, b) {
@@ -58,5 +56,5 @@ function subtract(a, b) {
 }
 
 function multiply(a, b) {
-    
+
 }
