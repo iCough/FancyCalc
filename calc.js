fd10dbff08d8cb25bb2c77f1ce2daf090be98b2f
josh <jeycuff@gmail.com>
2019-08-27 15:43:28 +0200

    added comments

--- calc.js
+++ calc.js
@@ -1,46 +1,36 @@
 
-
+var screen = document.getElementById("screen");
 const numPad = document.getElementById('numpad');
+
 numPad.addEventListener("click", numPadClickHandler);
 
+/**
+ * Display keeps protocol of the buttons that were pressed
+ */
+console.log(screen);
+screen.textContent = "afasd";
 
 
+/**
+ * processes the information, that was passed through 
+ * numPadClickHandler
+ * @ event
+ */
 function numPadClickHandler(event) {
+    console.log(event);
+    // console.log(event.target);
     const clickedButton = event.target;
+
     const num = parseInt(clickedButton.textContent);
-    console.log(num);
     return num;
 }
 
-// function pressedNumber() {
 
-// for (let number of nums) {
-
-//     const pressedNum = document.querySelector(".button" + number.innerHTML).innerHTML;
-
-//     // if pressed button is the same number as the one iterated through
-//     // then print that number
-//     if (pressedNum === number) {
-//         console.log(pressedNum);
-//     }
-// }
-// }
 
 /** 
  * Eine for-loop machen, in der der gedrückte button mit der 
  * jeweiligen Zahl abgeglichen wird.
 */
-const one = document.querySelector(".button1");
-const two = document.querySelector(".button2");
-const three = document.querySelector(".button3");
-const four = document.querySelector(".button4");
-const five = document.querySelector(".button5");
-const six = document.querySelector(".button6");
-const seven = document.querySelector(".button7");
-const eigth = document.querySelector(".button8");
-const nine = document.querySelector(".button9");
-const zero = document.querySelector(".button0");
-
 
 
 function add(a, b) {
